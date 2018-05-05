3fcf5e5cbacead6db145767a70d58cb2dffa259a
Akash <akash.darknight@gmail.com>
2018-05-05 14:05:00 +0530

    Main_Script

--- Trigger-word-detection.py
+++ Trigger-word-detection.py
@@ -26,3 +26,6 @@ x =graph_spectrogram("audio_examples/example_train.wav")
 __,,  datadata  ==  wavfilewavfile.read("audio_examples/example_train.wav")
 print("Time steps in audio recording before spectrogram", data[:,0].shape)
 print("Time steps in input after spectrogram", x.shape)
+
+Tx = 5511 # The number of time steps input to the model from the spectrogram
+n_freq = 101 # Number of frequencies input to the model at each time step of the spectrogram
