8ae416a31fa10c3ff7d362888c7c050c6b044f8d
Akash <akash.darknight@gmail.com>
2018-05-16 15:05:00 +0530

    Main_Script

--- Trigger-word-detection.py
+++ Trigger-word-detection.py
@@ -94,3 +94,10 @@ def is_overlapping(segment_time, previous_segments):
     ### END CODE HERE ###
 
     return overlap
+
+
+
+overlap1 = is_overlapping((950, 1430), [(2000, 2550), (260, 949)])
+overlap2 = is_overlapping((2305, 2950), [(824, 1532), (1900, 2305), (3424, 3656)])
+print("Overlap 1 = ", overlap1)
+print("Overlap 2 = ", overlap2)
