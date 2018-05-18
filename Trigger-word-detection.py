785203046f6eb2703199e0984c6661261f543b42
Akash <akash.darknight@gmail.com>
2018-05-18 15:05:00 +0530

    Main_Script

--- Trigger-word-detection.py
+++ Trigger-word-detection.py
@@ -101,3 +101,9 @@ overlap1 = is_overlapping((950, 1430), [(2000, 2550), (260, 949)])
 overlap2 = is_overlapping((2305, 2950), [(824, 1532), (1900, 2305), (3424, 3656)])
 print("Overlap 1 = ", overlap1)
 print("Overlap 2 = ", overlap2)
+
+'''
+Overlap 1 =  False
+Overlap 2 =  True
+'''
+
