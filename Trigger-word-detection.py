5fe48229d85f820befba9435bb8673ab74b53a4d
Akash <akash.darknight@gmail.com>
2018-05-28 15:05:00 +0530

    Main_Script

--- Trigger-word-detection.py
+++ Trigger-word-detection.py
@@ -157,3 +157,20 @@ def insert_audio_clip(background, audio_clip, previous_segments):
 
     # Expected audio
     IPython.display.Audio("audio_examples/insert_reference.wav")
+
+    # GRADED FUNCTION: insert_ones
+
+    def insert_ones(y, segment_end_ms):
+        """
+        Update the label vector y. The labels of the 50 output steps strictly after the end of the segment
+        should be set to 1. By strictly we mean that the label of segment_end_y should be 0 while, the
+        50 followinf labels should be ones.
+
+
+        Arguments:
+        y -- numpy array of shape (1, Ty), the labels of the training example
+        segment_end_ms -- the end time of the segment in ms
+
+        Returns:
+        y -- updated labels
+        """
