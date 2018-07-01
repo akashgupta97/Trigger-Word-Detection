8fb209f5c626de488fafa8bbb245fd75bb31ced6
Akash <akash.darknight@gmail.com>
2018-07-01 15:05:00 +0530

    Main_Script

--- Trigger-word-detection.py
+++ Trigger-word-detection.py
@@ -388,4 +388,21 @@ print("Dev set accuracy = ", acc)
 
 25/25 [==============================] - 4s
 Dev set accuracy =  0.94600725174
+
 '''
+
+
+def detect_triggerword(filename):
+    plt.subplot(2, 1, 1)
+
+    x = graph_spectrogram(filename)
+    # the spectogram outputs (freqs, Tx) and we want (Tx, freqs) to input into the model
+    x = x.swapaxes(0, 1)
+    x = np.expand_dims(x, axis=0)
+    predictions = model.predict(x)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(predictions[0, :, 0])
+    plt.ylabel('probability')
+    plt.show()
+    return predictions
