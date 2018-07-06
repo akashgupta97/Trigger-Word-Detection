b177faad4f446ab8e8106ec97bbebb987fb4d7ac
Akash <akash.darknight@gmail.com>
2018-07-06 15:05:00 +0530

    Main_Script

--- Trigger-word-detection.py
+++ Trigger-word-detection.py
@@ -443,3 +443,17 @@ def chime_on_activate(filename, predictions, threshold):
     prediction = detect_triggerword(filename)
     chime_on_activate(filename, prediction, 0.5)
     IPython.display.Audio("./chime_output.wav")
+
+
+# Trying different example
+
+    # Preprocess the audio to the correct format
+    def preprocess_audio(filename):
+        # Trim or pad audio segment to 10000ms
+        padding = AudioSegment.silent(duration=10000)
+        segment = AudioSegment.from_wav(filename)[:10000]
+        segment = padding.overlay(segment)
+        # Set frame rate to 44100
+        segment = segment.set_frame_rate(44100)
+        # Export as wav
+        segment.export(filename, format='wav')
